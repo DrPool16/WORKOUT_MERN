7e53df183bfa67c910935aef9503197780b4ae14
DrPool16 <elmesdepool@gmail.com>
2023-12-10 22:45:02 -0500

    Push frontend

---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,30 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+
+// date fns
+ import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+const WorkoutDetails = ({workout}) => {
+    const {dispatch} = useWorkoutsContext()
+    
+    const handleClick = async () => { 
+       const reponse = await fetch('/api/workouts/'+ workout._id,{
+        method: 'DELETE'
+       })
+       const json = await reponse.json()
+       if(reponse.ok){ 
+        dispatch({type: 'DELETE_WORKOUT', payload: json})
+       }
+    }
+    return(
+        <div className="workout-details">
+            <h4>{workout.title}</h4>
+            <p><strong>Load (kg): </strong>{workout.load}</p>
+            <p><strong>Reps: </strong>{workout.reps}</p>
+            <p>{formatDistanceToNow(new Date(workout.createdAt), {addSuffix: true})}</p>
+            <span className='material-symbols-outlined' onClick={handleClick}>delete_forever</span>
+        </div>
+    )
+}
+
+
+export default WorkoutDetails
